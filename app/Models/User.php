112f13d41b57ae6733b2c81bd43312bc2136f995
mohamed_bouth <mohamedbouth87@gmail.com>
2026-01-15 20:56:34 +0100

    JDP-5 create a user class

---
+++ app/Models/User.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace Models;
+
+class User extends BaseModel {
+    
+    protected array $attributes = [];
+    protected $tableName = 'users';
+
+    public function __set($key, $value)
+    {
+
+        if ($key === 'password') {
+            $value = password_hash($value, PASSWORD_BCRYPT);
+            $key = 'password_hash';
+        }
+        
+        $this->attributes[$key] = $value;
+    }
+
+    public function __get($key)
+    {
+        return $this->attributes[$key] ?? null;
+    }
+
+    protected function getTable(): string
+    {
+        return $this->tableName;
+    }
+
+    public function save()
+    {
+        return $this->create($this->attributes);
+    }
+}
